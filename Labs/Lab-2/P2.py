5ad7b0687098900602fd3192764ff5af410f5f94
LAPTOP-B5F9U8D1\Antonio <antonio.agj@gmail.com>
2023-11-13 12:36:23 +0100

    pylint

--- Labs/Lab-2/P2.py
+++ Labs/Lab-2/P2.py
@@ -2,6 +2,7 @@
     Este fichero contiene todos los métodos de la práctica
     Estos comentarios los utiliza pdoc3 para generar la documentación
     Dentro de un entorno virtual (Windows)
+    pylint P2.py --rc-file my_pylintrc
 """
 __author__ = "Antonio Garmendia"
 __copyright__ = "Copyright 2024"
@@ -34,3 +35,4 @@ if __name__ == '__main__':
     finish = time.time()
     print(f'El número de fibonacci es: {fibonacci_number}')
     print(f'El algoritmo tardó: {finish - start} en encontrar el número de Fibonacci para el número {nb}')
+    
